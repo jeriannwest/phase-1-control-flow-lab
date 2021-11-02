f407a6e4bda31a253358a658ac5b652aa0aa968c
+ <jeriannwest@yahoo.com>
2021-11-02 04:57:28 -0400

    commit tree fa7dbb5570f880cddf3c2e511c3ff4a917b01d6b

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  //let feet;
+  if (feet <= 400){
+    return 'This one is on me!';
+    }else if (feet > 2500) {
+      return "No can do.";
+  } else (feet> 2000) 
+    return 'I will gladly take your thirty bucks.';
+    } 
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){ 
+  return (city === "NYC"? "Ok, sounds good." : "No go.")
 }
-
-function switchOnCharmFromTip(){
   // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you."
+    default:
+    return "Bye."
+}
 }
